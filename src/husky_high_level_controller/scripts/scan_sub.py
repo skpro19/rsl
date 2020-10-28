6120f15f516b9cbcaab10bcdab2365e182f92f35
ShubhamKejriwal <shubhamkumarpro1@gmail.com>
2020-10-28 23:42:44 +0530

    c3.5

--- src/husky_high_level_controller/scripts/scan_sub.py
+++ src/husky_high_level_controller/scripts/scan_sub.py
@@ -2,12 +2,57 @@
 
 import rospy
 from sensor_msgs.msg import LaserScan 
-
+import math 
 
 def callback(data): 
 
+    range_arr = data.ranges 
+   
+    range_len = len(range_arr)
     
-    rospy.loginfo(f'data : {data}')
+    pos_list=  []
+    
+
+    ang_min = data.angle_min
+        
+    rospy.loginfo(f'ang_min : {ang_min}')
+
+    ang_inc = data.angle_increment 
+    
+    rospy.loginfo(f'ang_inc : {ang_inc}')
+
+    rospy.loginfo(f'range_len : {range_len}')
+
+    for idx, val in enumerate(range_arr, start = 1): 
+
+                #rospy.loginfo(f'idx : {idx} val: {val}')
+        
+
+        if not math.isinf(val): 
+            
+            curr_ang = ang_min + (idx * ang_inc)
+            
+            val_list = [] 
+
+            val_list.append(idx)
+            val_list.append(val)
+            val_list.append(curr_ang)
+            
+            pos_list.append(val_list)
+
+        
+    for elem in pos_list: 
+
+        for p in elem: 
+
+            rospy.loginfo(p)
+
+        rospy.loginfo(" ")
+
+
+                    
+            
+
 
 def listener():
 
